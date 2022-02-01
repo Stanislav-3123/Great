945f61afd6913c980aa8b7fe32a4652ce096fea7
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-02-01 23:37:53 +0400

    Операторы сравнения

---
+++ part-1/js/сomparison-perators.js
@@ -0,0 +1,7 @@
+let a = 5 > 4 // true
+let b = "ананас" > "яблоко" // false
+let c = "2" > "12" // true
+let e = undefined == null // true
+let g = undefined === null // false
+let h = null == "\no\n" // false
+let i = null === +"\no\n" // false
