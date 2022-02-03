cffbf39b6bfc7b16fa90e550aded727b5a1ea09a
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-02-04 00:36:17 +0400

    if-else and '?&:'

---
+++ part-1/js/if-else.js
@@ -0,0 +1,38 @@
+// let ansver = prompt('Какое "официальное назавание" JavaScript?',);
+// console.log(ansver);
+// Если пользователь вводит «ECMAScript», то показать: «Верно!», в противном случае – отобразить: «Не знаете? ECMAScript!»
+// if (ansver === 'ECMAScript') {
+// 	alert('Верно!');
+// } else if (ansver != 'ECMAScript' ) {
+// 	alert('Не знаете? ECMAScript!');
+// };
+// if (ansver === 'ECMAScript') {
+// 	alert('Верно!');
+// } else {
+// 	alert('Не знаете? ECMAScript!');
+// };
+// Используя конструкцию if..else, напишите код, который получает число через prompt, а затем выводит в alert:
+
+// 1, если значение больше нуля,
+// -1, если значение меньше нуля,
+// 0, если значение равно нулю.
+// Предполагается, что пользователь вводит только числа.
+
+// let value = prompt('Введите число',);
+
+// if (value > 0) {
+// 	alert(1);
+// } else if (value < 0) {
+// 	alert(-1);
+// } else {
+// 	alert(0);
+// };
+// let result = (a + b < 4) ? 'Мало' : 'Много'; 
+// console.log(result);
+
+// let message = (login == 'Сотрудник') ? 'Hello' : 
+// 	(login == 'Director') ? 'Good day' :
+// 	(login == '') ? 'No login' : ''; 
+
+
+
